2e96c5a78e73dc75221ccae6587bc1d27cedb25e
Drew Rowan <andrew.rowan@wallee.com>
2024-08-06 11:05:04 +0200

    WAL-242033_missing_payment_methods added new migration

---
+++ src/Migrations/CreateWebhooks2.php
@@ -0,0 +1,33 @@
+<?php
+namespace Wallee\Migrations;
+
+use Plenty\Modules\Payment\Method\Contracts\PaymentMethodRepositoryContract;
+use Wallee\Services\PaymentService;
+
+class CreateWebhooks2
+{
+    /**
+     *
+     * @var PaymentService
+     */
+    private $paymentService;
+
+    /**
+     * Constructor.
+     *
+     * @param PaymentService $paymentService
+     */
+    public function __construct(
+        PaymentService $paymentService
+    ) {
+        $this->paymentService = $paymentService;
+    }
+
+    /**
+     * Creates the payment methods for the whitelabel plugin.
+     */
+    public function run()
+    {
+        $this->paymentService->createWebhook();
+    }
+}
