c3424aea88261fa56f2bbe5f9d7276d2f1e8fa5b
Edgaras <edgaras.tkacenka@wallee.com>
2024-10-02 14:35:22 +0300

    Fixed

--- src/Methods/PostFinancePayPaymentMethod.php
+++ src/Methods/PostFinancePayPaymentMethod.php
@@ -15,6 +15,7 @@ class PostFinancePayPaymentMethod extends AbstractPaymentMethod
      */
     public function isActive(): bool
     {
+        return true;
         if ($this->configRepo->get('wallee.postfinancepay_active') == "true") {
             return true;
         } else {
