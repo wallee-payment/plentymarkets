da684abf0de445724eedf447a32d13c4868ed65c
andrewrowanwallee <andrew.rowan@wallee.com>
2025-05-07 10:58:13 +0200

    WAL-328592_routes use namespace and api router

--- src/Providers/WalleeRouteServiceProvider.php
+++ src/Providers/WalleeRouteServiceProvider.php
@@ -2,24 +2,30 @@
 namespace Wallee\Providers;
 
 use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\ApiRouter;
 use Plenty\Plugin\Routing\Router;
-use Wallee\Controllers\PaymentNotificationController;
-use Wallee\Controllers\PaymentProcessController;
-use Wallee\Controllers\PaymentTransactionController;
 
 class WalleeRouteServiceProvider extends RouteServiceProvider
 {
 
     /**
      *
-     * @param Router $router
+     * @param ApiRouter $apiRouter
      */
-    public function map(Router $router)
+    public function map(ApiRouter $apiRouter )
     {
-        $router->post('wallee/update-transaction', 'PaymentNotificationController@updateTransaction');
-        $router->get('wallee/fail-transaction/{id}', 'PaymentProcessController@failTransaction')->where('id', '\d+');
-        $router->post('wallee/pay-order', 'PaymentProcessController@payOrder');
-        $router->get('wallee/download-invoice/{id}', 'PaymentTransactionController@downloadInvoice')->where('id', '\d+');
-        $router->get('wallee/download-packing-slip/{id}', 'PaymentTransactionController@downloadPackingSlip')->where('id', '\d+');
+        $apiRouter->version(
+            ['v1'],
+            ['namespace' => 'Wallee\Controllers'],
+            function ($apiRouter) {
+
+                //Frontend routes
+                $apiRouter->post('wallee/update-transaction', 'PaymentNotificationController@updateTransaction');
+                $apiRouter->get('wallee/fail-transaction/{id}', 'PaymentProcessController@failTransaction')->where('id', '\d+');
+                $apiRouter->post('wallee/pay-order', 'PaymentProcessController@payOrder');
+                $apiRouter->get('wallee/download-invoice/{id}', 'PaymentTransactionController@downloadInvoice')->where('id', '\d+');
+                $apiRouter->get('wallee/download-packing-slip/{id}', 'PaymentTransactionController@downloadPackingSlip')->where('id', '\d+');
+            }
+        );
     }
 }
