4573c3dcbbec64098e1d10657d2f4a90ba2ab79d
Andrii Nidodaiev <andrii.nidodaiev@wallee.com>
2026-05-14 14:53:17 +0300

    WAL-450993: test failedUrl removal for checkout

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -221,7 +221,8 @@ class PaymentService
                 'language' => $this->session->getLocaleSettings()->language,
                 'successUrl' => $this->getSuccessUrl(),
                 'failedUrl' => $this->getFailedUrl(),
-                'checkoutUrl' => $this->getFailedUrl()
+//                'checkoutUrl' => $this->getFailedUrl(),
+                'checkoutUrl' => $this->getCheckoutUrl()
             ];
             
             // Only add transactionId if it exists (not null)
