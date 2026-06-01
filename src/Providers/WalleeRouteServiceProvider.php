596d79f03cbe5083f40aa72162ac9c2629a7a247
Alberto G. Viu <alberto.viu@wallee.com>
2026-06-01 12:05:35 +0200

    PWA: comment out the webhook that listens with trailing slash

--- src/Providers/WalleeRouteServiceProvider.php
+++ src/Providers/WalleeRouteServiceProvider.php
@@ -20,10 +20,10 @@ class WalleeRouteServiceProvider extends RouteServiceProvider
             'rest/v1/wallee/update-transaction',
             'Wallee\Controllers\PaymentNotificationController@updateTransaction',
         );
-        $router->post(
-            'rest/v1/wallee/update-transaction/',
-            'Wallee\Controllers\PaymentNotificationController@updateTransaction',
-        );
+        // $router->post(
+        //     'rest/v1/wallee/update-transaction/',
+        //     'Wallee\Controllers\PaymentNotificationController@updateTransaction',
+        // );
         $router->get('wallee/fail-transaction/{id}', 'Wallee\Controllers\PaymentProcessController@failTransaction')->where('id', '\d+');
         $router->post('wallee/pay-order', 'Wallee\Controllers\PaymentProcessController@payOrder');
         $router->get('wallee/download-invoice/{id}', 'Wallee\Controllers\PaymentTransactionController@downloadInvoice')->where('id', '\d+');
