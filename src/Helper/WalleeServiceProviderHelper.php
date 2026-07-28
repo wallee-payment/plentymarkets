14db2ad4c711adf3afccdcb3289b7f2d58c09848
andrewrowanwallee <andrew.rowan@wallee.com>
2026-07-28 09:54:52 +0200

    add logs

--- src/Helper/WalleeServiceProviderHelper.php
+++ src/Helper/WalleeServiceProviderHelper.php
@@ -96,6 +96,7 @@ class WalleeServiceProviderHelper
      */
     private function isPwaContext(): bool
     {
+        $this->getLogger(__METHOD__)->error('eventlistener', "here");
         $originUrl = $this->session->getPlugin()->getValue('walleeOriginUrl');
         $transactionId = $this->session->getPlugin()->getValue('walleeTransactionId');
 
